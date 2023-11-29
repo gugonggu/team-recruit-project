a9c7a629518e1a89ea7e1015bd521d5fa5ffc76f
hongchun <jhc5317@gmail.com>
2023-11-29 21:26:40 +0900

    프록시 테스트

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,8 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        "/api",
-        createProxyMiddleware({
+        createProxyMiddleware("/api", {
             target: "https://team-recruit-project.onrender.com",
             changeOrigin: true,
         })
