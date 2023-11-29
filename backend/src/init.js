14dc45a05e6027b24ff242677b725975e973ab5f
hongchun <jhc5317@gmail.com>
2023-11-29 14:55:51 +0900

    헤로쿠 세팅

--- backend/src/init.js
+++ backend/src/init.js
@@ -2,6 +2,6 @@ import "dotenv/config";
 import "./db.js";
 import app from "./server.js";
 
-app.listen(process.env.PORT, () =>
-    console.log(`Server listening on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 4000, () =>
+    console.log(`Server listening on port ${process.env.PORT || 4000}`)
 );
